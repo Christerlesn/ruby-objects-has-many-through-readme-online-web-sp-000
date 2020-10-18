878cb784ffa75046a11960547aa18543862729a4
Christerle Nounoute <christerlesn@gmail.com>
2020-10-18 15:52:12 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -22,7 +22,10 @@ attr_accessor :name, :years
   end
 
   def best_tipper
-
+    meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+    best_tipped_meal.customer
   end
 
 end #end of class Waiter
